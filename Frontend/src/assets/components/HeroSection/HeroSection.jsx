18a0c18e658cd31ddbab66c2744fe063dff8391c
M Billy Hasman <billyhasman08@gmail.com>
2025-02-24 20:57:04 +0700

    feat: Add Content Header

--- Frontend/src/assets/components/HeroSection/HeroSection.jsx
+++ Frontend/src/assets/components/HeroSection/HeroSection.jsx
@@ -2,10 +2,16 @@ import HeroImg from '../../img/hero-img.png'
 
 const HeroSection = () => {
   return (
-    <div className='relative flex items-center bg-cover flex-start bg-center h-screen w-full text-left' style={{ backgroundImage: `url(${HeroImg})` }}>
-      <div className='absolute top-0 right-0 bottom-0 left-0'>
-        <main className='px-10'></main>
-      </div>
+    <div className='relative flex items-center bg-cover flext-start bg-center text-left h-svh w-full' style={{ backgroundImage: `url(${HeroImg})` }}>
+      <div className='absolute top-0 right-0 bottom-0 left-0'></div>
+      <main className='px-10 lg:px-24 z-10'>
+        <div className='text-left'>
+          <h2 className='text-3xl text-white'>T-shirt / Tops</h2>
+        </div>
+        <p className='mt-3 text-white sm:mt-5 sm:max-w-xl text-6xl'>Summer Value Pack</p>
+        <p className='mt-3 text-white sm:mt-5 sm:max-w-xl text-2xl'>cool / colorfu / comfy</p>
+        <button className='border-2 rounded mt-6 border-black hover:bg-white hover:text-black hover:border-black transition-all duration-300 ease-in-out text-white bg-black w-44 h-12'>Shop Now</button>
+      </main>
     </div>
   )
 }
